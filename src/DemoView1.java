aba7bafd7672ef7624e221181aec69ee45c8b5a3
wangtie <tie.wang@dragonflow.com>
2012-08-15 09:06:32 +0800

    test   demo

---
+++ src/DemoView1.java
@@ -0,0 +1,23 @@
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+
+public class DemoView1 extends ViewPart {
+
+	public DemoView1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
